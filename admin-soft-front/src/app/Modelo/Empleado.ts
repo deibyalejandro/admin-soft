2ed3c9e0d2c109f06cd31b26a4353424294d9ff3
deibyalejo99 <deibyalejo123@gmail.com>
2022-09-02 18:41:34 -0500

    Front y postman collection

---
+++ admin-soft-front/src/app/Modelo/Empleado.ts
@@ -0,0 +1,14 @@
+export class Empleado{
+    id:number;
+    documentoTipo:string;
+    documentoNumero:number;
+    nombres:string;
+    apellidos:string;
+    departamentoId:number;
+    ciudad:string;
+    direccion:string;
+    correoElectronico:string;
+    telefono:number;
+    fechaCreacion:Date;
+    fechaModificacion:Date;
+}
---
+++ admin-soft-front/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ListarComponent } from './empleado/listar/listar.component';
+
+
+const routes: Routes = [
+  {path:'listar', component:ListarComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
